050df027bc4fedc5a251e37e2c1bd2907879ee2e
Gustavo400 <Gustavo4@live.unc.edu>
2022-04-03 00:10:24 -0400

    Maybe this will work

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ fs.readFile('./www/index.html', 'utf8', (err, data) => {
         res.setHeader('Content-Type', 'test/html')
         res.end(data)
     })
-
     // Start the `server` const listening on the port defined by argument in your `port` const. 
     // Put the exact message `Server listening on port ${port}` on the console log. 
     server.listen(port, () => {
